a2bb7ba6966389a4306b701c166153e42a3515ae
Zhen Liu <2011371871@qq.com>
2026-02-13 08:57:15 +0000

    Fri, Feb 13, 2026, 4:57 PM +08:00

--- ch_01/words.py
+++ ch_01/words.py
@@ -1,5 +1,9 @@
 words = {}
 
 with open("words.txt") as f:
-    f.read().split()
+    total = [w.strip() for w in f.read().split()]
+
+for item in total:
+    words[item] = words.get(item, 0) + 1
+
 
