953dbc111ba737661422ef29a3cace5fec84c626
Zhen Liu <2011371871@qq.com>
2026-02-13 08:59:46 +0000

    Fri, Feb 13, 2026, 4:59 PM +08:00

--- ch_01/words.py
+++ ch_01/words.py
@@ -3,7 +3,11 @@ words = {}
 with open("words.txt") as f:
     total = [w.strip() for w in f.read().split()]
 
+common = ""
+max = 0
 for item in total:
     words[item] = words.get(item, 0) + 1
+    if words[item] > max:
+        common, max = item, words[item]
 
-
+print(common)
