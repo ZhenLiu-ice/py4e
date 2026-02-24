474661641a2f83b8b2323448974476716c4279e5
Zhen Liu <2011371871@qq.com>
2026-02-24 08:44:17 +0000

    Tue, Feb 24, 2026, 4:44 PM +08:00

--- ch_04/buildin_func.py
+++ ch_04/buildin_func.py
@@ -6,18 +6,19 @@ print(repr(max(s)))
 print(repr(min(s)))
 print(len(s))
 
-
-# 获取所有内置函数
 builtin_funcs = [
-    name
-    for name in dir(builtins)
+    name for name in dir(builtins)
     if inspect.isbuiltin(getattr(builtins, name)) and not name.startswith("_")
 ]
 
-for i, func in enumerate(sorted(builtin_funcs), start=1):
-    if i % 5 != 0:
-        print(func, end="\t")
-    else:
-        print(func)
+# 计算最长函数名，确定列宽
+max_len = max(len(name) for name in builtin_funcs)
+col_width = max_len + 2  # 加一些间距
 
+# 每行显示5列
+cols = 5
+for i, func in enumerate(sorted(builtin_funcs), start=1):
+    print(func.ljust(col_width), end="")
+    if i % cols == 0:
+        print()
 print()
