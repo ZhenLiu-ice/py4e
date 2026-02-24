b2420af521cb35e70f8b32a53c5dd4189c8a80f5
Zhen Liu <2011371871@qq.com>
2026-02-24 08:35:27 +0000

    Tue, Feb 24, 2026, 4:35 PM +08:00

--- ch_04/buildin_func.py
+++ ch_04/buildin_func.py
@@ -1,6 +1,17 @@
+import inspect
+import builtins
+
 s = "hello world"
 print(repr(max(s)))
 print(repr(min(s)))
 print(len(s))
 
-print(dir(__buildins__))
+
+# 获取所有内置函数
+builtin_funcs = [
+    name for name in dir(builtins) if inspect.isbuiltin(getattr(builtins, name))
+]
+
+# 按字母排序并打印
+for func in sorted(builtin_funcs):
+    print(func)
