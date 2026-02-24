3fa71cef6ccf2029d9f0e26118fcb920a14dbef7
Zhen Liu <2011371871@qq.com>
2026-02-24 08:42:30 +0000

    Tue, Feb 24, 2026, 4:42 PM +08:00

--- ch_04/buildin_func.py
+++ ch_04/buildin_func.py
@@ -9,12 +9,15 @@ print(len(s))
 
 # 获取所有内置函数
 builtin_funcs = [
-    name for name in dir(builtins) if inspect.isbuiltin(getattr(builtins, name))
+    name
+    for name in dir(builtins)
+    if inspect.isbuiltin(getattr(builtins, name)) and not name.startswith("_")
 ]
 
-for i, func in enumerate(sorted(builtin_funcs)):
-    if i % 9 !=0 and  not func.startswith("_"):
-        print(func, end='\t')
+for i, func in enumerate(sorted(builtin_funcs), start=1):
+    if i % 5 != 0:
+        print(func, end="\t")
     else:
         print(func)
+
 print()
